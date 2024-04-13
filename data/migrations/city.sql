ce940fd2dc3a5471d3d5e5ec00c94cfab9f52a03
sashabodiul <sashabodiul07@gmail.com>
2024-04-13 08:17:09 +0300

    update migrations

--- data/migrations/city.sql
+++ data/migrations/city.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS city (
     id INT AUTO_INCREMENT PRIMARY KEY,
     G_id VARCHAR(255),
     `location` VARCHAR(255), -- Enclosed 'location' in backticks instead of double quotes
-    link VARCHAR(1000) UNIQUE,
+    link VARCHAR(255) UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
